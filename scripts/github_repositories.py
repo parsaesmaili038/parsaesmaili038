6f308d6ea25e136ed52fe00d89525165d7c76f08
Parsa esmaili <293425477+the-par3a@users.noreply.github.com>
2026-09-01 17:26:38 +0330

    Update github_repositories.py

--- scripts/github_repositories.py
+++ scripts/github_repositories.py
@@ -9,7 +9,7 @@ if hasattr(sys.stdout, "reconfigure"):
     sys.stdout.reconfigure(encoding="utf-8")
 
 if hasattr(sys.stderr, "reconfigure"):
-    sys.stderr.reconfigure(encoding="utf-8"
+    sys.stderr.reconfigure(encoding="utf-8")
 
 
 # =========================
